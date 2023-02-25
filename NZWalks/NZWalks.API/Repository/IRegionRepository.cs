76cd3e8f97b648006e1e0ac83f1175c1dc55de6d
RaviRanjansingh1989 <raviranjansingh1989@gmail.com>
2023-02-25 18:04:07 +0530

    Async method calledAsync AddedMigratedSQLin API

---
+++ NZWalks/NZWalks.API/Repository/IRegionRepository.cs
@@ -0,0 +1,10 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
+{
+    public interface IRegionRepository
+    {
+       Task<IEnumerable<Region>> GetAllAsync();
+       Task<Region> GetAsync(Guid id);
+    }
+}
