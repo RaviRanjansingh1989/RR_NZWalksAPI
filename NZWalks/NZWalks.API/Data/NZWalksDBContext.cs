6934fabf616e22b09ebe11e1316a099043cc7ed6
RaviRanjansingh1989 <raviranjansingh1989@gmail.com>
2023-02-25 18:04:07 +0530

    Async method calledAsync AddedMigratedSQLin API

---
+++ NZWalks/NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walk { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repository/RegionRepository.cs
@@ -0,0 +1,18 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext nZWalksDBContext;
+        public RegionRepository(NZWalksDBContext nZWalksDBContext)
+        {
+            this.nZWalksDBContext = nZWalksDBContext;
+        }
+        public IEnumerable<Region> GetAll()
+        {
+            return nZWalksDBContext.Region.ToList();
+        }
+    }
+}
