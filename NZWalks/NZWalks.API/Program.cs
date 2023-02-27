4e557b68fd62bef19a26107f56a080ca4fcb9efe
RaviRanjansingh1989 <raviranjansingh1989@gmail.com>
2023-02-27 10:42:32 +0530

    Revert "Async method calledAsync AddedMigratedSQLin API"
    This reverts commit 76cd3e8f97b648006e1e0ac83f1175c1dc55de6d.

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using NZWalks.API.Data;
-using NZWalks.API.Repository;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,12 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<NZWalksDBContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
-});
-builder.Services.AddScoped<IRegionRepository, RegionRepository>();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
